b86e771dd3fc3606cd9c0308bb089d760c0087f7
StarsFu <1170741585@qq.com>
2019-11-19 20:50:24 +0800

    network

--- scripts/starrobot_network.sh
+++ scripts/starrobot_network.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-
-echo "#它应该是你虚拟机的IP" >> ~/.bashrc
+echo "#It should be the IP of your virtual machine" >> ~/.bashrc
 echo "export ROS_HOSTNAME=192.168.1.104" >> ~/.bashrc
-echo "#使用远程虚拟机作为机器人的主机它应该是虚拟机的IP" >> ~/.bashrc
-echo "#使用树莓派或者工控机作为机器人主机，那么它应该是树莓派或者工控机的IP" >> ~/.bashrc
+echo "#If you are using a remote virtual machine as the host of a robot it should be the IP of the virtual machine" >> ~/.bashrc
+echo "#If you use raspberry PI or ipc as the robot host, it should be the IP of raspberry PI or ipc" >> ~/.bashrc
 echo "export ROS_MASTER_URI=http://192.168.1.102:11311" >> ~/.bashrc
-
 echo "# a1 a2 d2 d4 o3 o4 m4 t2 t4" >> ~/.bashrc
 echo "export STARROBOTBASE=d2" >> ~/.bashrc
 echo "# rplidar ydlidar" >> ~/.bashrc
